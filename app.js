4cfd94b118922c9c9b1481924f41dd46cc8181ff
ddboy19912 <99345157+ddboy19912@users.noreply.github.com>
2022-12-06 10:11:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+require('dotenv/config');
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+var database;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to MongoDB Api');
+});
+
+app.get('/api/books', (req, res) => {
+  database
+    .collection('ATAfrica2')
+    .find({})
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+});
+
+app.listen(8080, () => {
+  MongoClient.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true },
+    (error, result) => {
+      if (error) throw error;
+      database = result.db('Test');
+      console.log('Connection Successful');
+    }
+  );
+});
